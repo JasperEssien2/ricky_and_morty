5aafaaa0223258caf7b16489b91464f547094090
Jahswill Essien <jasperessien2@gmail.com>
2023-02-12 19:47:06 +0100

    delete returns bool

---
+++ lib/domain/repository.dart
@@ -0,0 +1,9 @@
+import 'package:ricky_and_morty/domain/character_entity.dart';
+
+abstract class Repository {
+  Future<List<CharacterEntity>> fetch();
+
+  Future<bool> deleteFavourite(int id);
+
+  Future<bool> addFavourite(int id);
+}
